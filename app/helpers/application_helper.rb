4ab95f3bef707b54cacf7fcf2c56a7974991c95e
verlitas <melodykirshberg@gmail.com>
2020-08-31 20:23:11 -0700

    adding some styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
   def login_helper 
     if current_user.is_a?(GuestUser)
-      (link_to "Register", new_user_registration_path) +
+      (link_to "Register", new_user_registration_path, class: 'btn btn-sm btn-outline-secondary') +
       " ".html_safe + 
-      (link_to "Login", new_user_session_path)
+      (link_to "Login", new_user_session_path, class: 'btn btn-sm btn-outline-secondary')
     else
-      link_to "Logout", destroy_user_session_path, method: :delete
+      (link_to "Logout", destroy_user_session_path, method: :delete, class: 'btn btn-sm btn-outline-secondary')
     end
   end
 end
